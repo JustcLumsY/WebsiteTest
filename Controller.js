ce1a66b264e3e7697bc2142969d588b0c1966276
John Espen Thoresen <84008816+JustcLumsY@users.noreply.github.com>
2021-08-11 13:56:45 +0200

    Add files via upload

--- Controller.js
+++ Controller.js
@@ -3,6 +3,14 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
   }
   
-  function closeNav() {
+function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
+
+  function LogIn() {
+    alert("Hey");
+    document.getElementById("loginSite").innerHTML;
+  
+
+  }
+  
--- View.js
+++ View.js
@@ -18,11 +18,16 @@ show();
 function show() {
     document.getElementById('mainPage').innerHTML = `
 
-        <header id="pageHeader" >Header</header>
+        <header id="pageHeader">
+        <button id="loginBtn" onclick="LogIn()">Login</button>
+        
+        <h1>ButikkensNavn</h1></header>
+
         <article id="mainArticle">Article</article>
 
         <nav id="mainNav">
-        <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; Menu</span>
+        <span style="font-size:30px;cursor:pointer" 
+        onclick="openNav()">&#9776; Menu</span>
         <div id="mySidenav" class="sidenav">
             <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
             <a href="#">Home</a>
@@ -34,7 +39,31 @@ function show() {
 
         <div id="siteSale">Sale</div>
 
-        <footer id="pageFooter">Footer</footer>
+        
         
     `;
 }
+
+function LogIn() {
+    document.getElementById('loginSite').innerHTML = `
+   
+     <h1> Login Form </h1> 
+    <form>  
+        <div class="container">   
+            <label>Username : </label>   
+            <input type="text" placeholder="Enter Username" name="username" required>  
+            <label>Password : </label>   
+            <input type="password" placeholder="Enter Password" name="password" required>  
+            <button type="submit">Login</button>   
+            <input type="checkbox" checked="checked"> Remember me   
+            <button type="button" class="cancelbtn"> Cancel</button>   
+            Forgot <a href="#"> password? </a>   
+        </div>   
+    </form>     
+   
+    `;
+}
+
+
+
+{/* <footer id="pageFooter">Footer</footer> */}
